a2b0b2fc0fb8f721b03709289b64c68c0c86a4c8
Bron LaPoint <gemini2022@yahoo.com>
2023-01-10 12:43:12 -0500

    Working on publish manager form

---
+++ Manager.Application/PublishItems/GetPublishItems/Queries/GetPublishItemsQuery.cs
@@ -0,0 +1,7 @@
+﻿using MediatR;
+using Shared.Common.Classes;
+
+namespace Manager.Application.PublishItems.GetPublishItems.Queries
+{
+    public sealed record GetPublishItemsQuery() : IRequest<Result>;
+}
