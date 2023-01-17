69eae45caf4a9485a2b0a7aad5d3204137df63c4
Gabe LaPoint <glapoint22@gmail.com>
2023-01-17 12:15:15 -0500

    Worked on move

--- Manager.Application/Products/MoveProduct/Commands/MoveProductCommandHandler.cs
+++ Manager.Application/Products/MoveProduct/Commands/MoveProductCommandHandler.cs
@@ -1,26 +1,67 @@
 ﻿using Manager.Application.Common.Interfaces;
-using Manager.Domain.Entities;
 using MediatR;
+using Microsoft.EntityFrameworkCore;
 using Shared.Common.Classes;
+using Website.Application.Common.Interfaces;
 
 namespace Manager.Application.Products.MoveProduct.Commands
 {
     public sealed class MoveProductCommandHandler : IRequestHandler<MoveProductCommand, Result>
     {
         private readonly IManagerDbContext _dbContext;
+        private readonly IWebsiteDbContext _websiteDbContext;
 
-        public MoveProductCommandHandler(IManagerDbContext dbContext)
+        public MoveProductCommandHandler(IManagerDbContext dbContext, IWebsiteDbContext websiteDbContext)
         {
             _dbContext = dbContext;
+            _websiteDbContext = websiteDbContext;
         }
 
         public async Task<Result> Handle(MoveProductCommand request, CancellationToken cancellationToken)
         {
-            Product productToBeMoved = (await _dbContext.Products.FindAsync(request.ItemToBeMovedId))!;
-
+            Domain.Entities.Product productToBeMoved = (await _dbContext.Products.FindAsync(request.ItemToBeMovedId))!;
             productToBeMoved.SubnicheId = request.DestinationItemId;
-
             await _dbContext.SaveChangesAsync();
+
+            Website.Domain.Entities.Product? websiteProduct = await _websiteDbContext.Products.FindAsync(request.ItemToBeMovedId);
+
+            // If the product exists on website
+            if (websiteProduct != null)
+            {
+                // Does the subniche exists on website?
+                if (!await _websiteDbContext.Subniches.AnyAsync(x => x.Id == request.DestinationItemId))
+                {
+                    Domain.Entities.Subniche subniche = (await _dbContext.Subniches.FindAsync(request.DestinationItemId))!;
+
+                    // Does the niche exists on website?
+                    if (!await _websiteDbContext.Niches.AnyAsync(x => x.Id == subniche.NicheId))
+                    {
+                        Domain.Entities.Niche niche = (await _dbContext.Niches.FindAsync(subniche.NicheId))!;
+
+                        // Add the niche to website
+                        _websiteDbContext.Niches.Add(new Website.Domain.Entities.Niche
+                        {
+                            Id = niche.Id,
+                            Name = niche.Name,
+                            UrlName = niche.UrlName
+                        });
+                    }
+
+                    // Add the subniche to website
+                    _websiteDbContext.Subniches.Add(new Website.Domain.Entities.Subniche
+                    {
+                        Id = subniche.Id,
+                        NicheId = subniche.NicheId,
+                        Name = subniche.Name,
+                        UrlName = subniche.UrlName
+                    });
+                }
+
+                // Update the product
+                websiteProduct.SubnicheId = request.DestinationItemId;
+                await _websiteDbContext.SaveChangesAsync();
+            }
+
             return Result.Succeeded();
         }
     }
--- Manager.Application/Subniches/MoveSubniche/Commands/MoveSubnicheCommandHandler.cs
+++ Manager.Application/Subniches/MoveSubniche/Commands/MoveSubnicheCommandHandler.cs
@@ -1,26 +1,51 @@
 ﻿using Manager.Application.Common.Interfaces;
-using Manager.Domain.Entities;
 using MediatR;
+using Microsoft.EntityFrameworkCore;
 using Shared.Common.Classes;
+using Website.Application.Common.Interfaces;
 
 namespace Manager.Application.Subniches.MoveSubniche.Commands
 {
     public sealed class MoveSubnicheCommandHandler : IRequestHandler<MoveSubnicheCommand, Result>
     {
         private readonly IManagerDbContext _dbContext;
+        private readonly IWebsiteDbContext _websiteDbContext;
 
-        public MoveSubnicheCommandHandler(IManagerDbContext dbContext)
+        public MoveSubnicheCommandHandler(IManagerDbContext dbContext, IWebsiteDbContext websiteDbContext)
         {
             _dbContext = dbContext;
+            _websiteDbContext = websiteDbContext;
         }
 
         public async Task<Result> Handle(MoveSubnicheCommand request, CancellationToken cancellationToken)
         {
-            Subniche subnicheToBeMoved = (await _dbContext.Subniches.FindAsync(request.ItemToBeMovedId))!;
-
+            Domain.Entities.Subniche subnicheToBeMoved = (await _dbContext.Subniches.FindAsync(request.ItemToBeMovedId))!;
             subnicheToBeMoved.NicheId = request.DestinationItemId;
-
             await _dbContext.SaveChangesAsync();
+
+            Website.Domain.Entities.Subniche? websiteSubniche = await _websiteDbContext.Subniches.FindAsync(request.ItemToBeMovedId);
+
+            if (websiteSubniche != null)
+            {
+                // Does the niche exists on website?
+                if (!await _websiteDbContext.Niches.AnyAsync(x => x.Id == request.DestinationItemId))
+                {
+                    Domain.Entities.Niche niche = (await _dbContext.Niches.FindAsync(request.DestinationItemId))!;
+
+                    // Add the niche to website
+                    _websiteDbContext.Niches.Add(new Website.Domain.Entities.Niche
+                    {
+                        Id = niche.Id,
+                        Name = niche.Name,
+                        UrlName = niche.UrlName
+                    });
+                }
+
+                // Update the subniche
+                websiteSubniche.NicheId = request.DestinationItemId;
+                await _websiteDbContext.SaveChangesAsync();
+            }
+
             return Result.Succeeded();
         }
     }
